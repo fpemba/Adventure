967876fd207a1a294dc5ced8a6691acbf8db7e2e
fpemba <fatuma.pemba@yahoo.com>
2021-01-05 21:32:25 -0500

    created location map

---
+++ src/org/wecancodeit/Main.java
@@ -0,0 +1,16 @@
+package org.wecancodeit;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Main {
+
+    private static Map<Integer,Location> locations = new HashMap<Integer,Location>();
+
+    public static void main(String[] args) {
+	Scanner scanner = new Scanner(System.in);
+
+	locations.put(0,new Location(0,"You are sitting in front of a computer learning Java"));
+    }
+}
